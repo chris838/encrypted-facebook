5ac48560aac88db852c39ec7f41630ffdd582608
Chris Harding <ch458@cam.ac.uk>
2010-10-28 16:09:28 +0100

    Deletions

--- src/defaults/preferences/prefs.js
+++
@@ -1,6 +0,0 @@
-pref("extensions.encryptedfacebook.boolpref", false);
-pref("extensions.encryptedfacebook.intpref", 0);
-pref("extensions.encryptedfacebook.stringpref", "A string");
-
-// https://developer.mozilla.org/en/Localizing_extension_descriptions
-pref("extensions.encrypted_facebook@chris.com.description", "chrome://encryptedfacebook/locale/overlay.properties");
