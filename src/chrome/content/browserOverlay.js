66fce855fe3a269fc09d9cdeb4e0c01ac10c2c8b
Chris Harding <ch458@cam.ac.uk>
2010-10-29 16:12:27 +0100

    Toolbar with dummy buttons and text box added

--- src/chrome/content/browserOverlay.js
+++ src/chrome/content/browserOverlay.js
@@ -13,9 +13,10 @@ EncryptedFacebookChrome.BrowserOverlay = {
    * Says 'Hello' to the user.
    */
   sayHello : function(aEvent) {
-    let stringBundle = document.getElementById("encryptedfacebook-string-bundle");
-    let message = stringBundle.getString("encryptedfacebook.greeting.label");
 
-    window.alert(message);
+    var result = window.prompt("Enter submission text");
+    
+    window.alert( result );
+
   }
 };
