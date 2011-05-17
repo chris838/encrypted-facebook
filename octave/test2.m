7e95cdcb38eb9e77473d4dcc9c14750b042ee23e
Chris Harding <ch458@cam.ac.uk>
2011-05-17 11:35:11 +0100

    nearly

--- octave/test2.m
+++ octave/test2.m
@@ -1,7 +1,12 @@
+for zqf=85:85
+
+
 # Generate an initial bitmask from the quantisation matrix
 
 # First read in a sample Facebook image
-sam = jpeg_read('sample.jpg');
+sam1 = imread('sample.jpg');
+imwrite(sam1, 'sample2.jpg', 'Quality', zqf);
+sam = jpeg_read('sample2.jpg');
 
 # Get the quant matrix for the luminance
 q = sam.quant_tables{sam.comp_info(1).quant_tbl_no};
@@ -18,11 +23,28 @@ m = bitor( m, 2*m );
 format hex;
 m = bitand(m , ones(8)*0x007f );
 %m(1,1) = 0;
-m
+m;
+
+
 
 # Load an 8x8 greyscale template jpeg and set its quant values
 i =  jpeg_read('template.jpg');
 i.quant_tables{1} = q;
+
+
+
+
+
+
+zz = 1000;
+ze=0;
+
+for zi=1:zz
+
+
+
+
+
 # Also set its coefficients to zero
 i.coef_arrays{1} = zeros(8);
 
@@ -40,7 +62,7 @@ i.coef_arrays{1} = quantize(d-off,q);
 # Perform JPEG compression
 jpeg_write(i, 'test.jpg');
 temp = imread("test.jpg");
-imwrite(temp, 'test2.jpg', 'Quality', 85);
+imwrite(temp, 'test2.jpg', 'Quality', zqf);
 j = jpeg_read('test2.jpg');
 
 # Retrieve data
@@ -62,8 +84,8 @@ w=0;
 for k=1:8
 	for l=1:8	
 		val = m(k,l);
-		for i=1:16
-		   if bitget( val, i ) == 1
+		for vv=1:16
+		   if bitget( val, vv ) == 1
 		       w = w + 1;
 		   end
 		end
@@ -72,5 +94,14 @@ end
 
 [e1 e2] = biterr( d, d2 );
 
-e1 / w
 
+
+
+
+ze = ze + ((e1 / w)/zz);
+
+end
+
+ze
+
+end
--- octave/test3.m
+++ octave/test3.m
@@ -1,12 +1,25 @@
+for qf=80:90
+
+z = 10000;
+e=0;
+
+for i=1:z
+
 # Create some random data 
 d = uint8( round(256*rand(8,8)) );
 
 # Perform JPEG compression
 nothing =imread('test.jpg');
-imwrite(d, 'test.jpg', 'Quality', 85);
+imwrite(d, 'test.jpg', 'Quality', qf);
 d2 = imread('test.jpg');
 
 # Calculate bit errors
 [e1 e2] = biterr( d, d2 );
 
-e2
+e = e + (e2/z);
+
+end
+
+e
+
+end
